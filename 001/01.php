b28d03cb1443afcfb3c96410c1810f6b93591469
Quynh Dang <quyynhdaang>
2018-11-16 18:57:07 +0700

    hi

---
+++ 001/01.php
@@ -0,0 +1,19 @@
+<div class="content">
+<?php
+echo "hello world!"
+?>
+</div>
+<style type="text/css">
+    *{
+        margin:0px;
+        padding:0;
+    }
+    .content{
+        width: 600px;
+        height:300px;
+        background:pink;
+        margin:10px auto;
+        line-height:300px;
+        text-align: center;
+    }
+</style>
